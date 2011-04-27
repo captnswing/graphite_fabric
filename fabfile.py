00b3b185b50f8688bc76bdab67e7453286c7b92b
captnswing <frank.hoffsummer@gmail.com>
2011-04-27 10:46:44 +0200

    fixed bashrc virtualenvwrapper, graphite.conf. latest fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from fabric.api import *
-from fabric.context_managers import cd, hide, settings
-from fabric.contrib.files import append, exists, sed
+from fabric.context_managers import cd
+from fabric.contrib.files import append, sed
 import os
-
-import time
 
 def stage():
     env.hosts = ['ec2-79-125-29-204.eu-west-1.compute.amazonaws.com']
@@ -15,29 +12,30 @@ def stage():
     env.key_filename = os.path.join(os.path.expanduser('~'), '.ssh', 'svti-frank.pem')
     env.virtualenv_home = '/opt/virtualenvs'
     env.venv_name = 'test'
-    
+
 def install_virtualenv():
     sudo('easy_install pip')
     sudo('pip install virtualenv virtualenvwrapper')
     sudo('mkdir -p %(virtualenv_home)s' % env)
     sudo('chown -R %(user)s %(virtualenv_home)s' % env)
     VIRTUALENV_BASHRC="""
-    test -r /usr/bin/virtualenvwrapper.sh && . /usr/bin/virtualenvwrapper.sh
-    export WORKON_HOME=%(virtualenv_home)s
-    export PIP_VIRTUALENV_BASE=$WORKON_HOME
-    # To tell pip to only run if there is a virtualenv currently activated, and to bail if not, use:
-    export PIP_REQUIRE_VIRTUALENV=true""" % env
+        test -r /usr/bin/virtualenvwrapper.sh && . /usr/bin/virtualenvwrapper.sh
+        export WORKON_HOME=%(virtualenv_home)s
+        export PIP_VIRTUALENV_BASE=$WORKON_HOME
+        # To tell pip to only run if there is a virtualenv currently activated, and to bail if not, use:
+        export PIP_REQUIRE_VIRTUALENV=true
+    """ % env
     bashrc_lines = [ l.strip() for l in VIRTUALENV_BASHRC.splitlines() if l.strip() ]
     append('~/.bashrc', bashrc_lines)
     with cd('%(virtualenv_home)s' % env):
         run('mkvirtualenv --no-site-packages %(venv_name)s' % env)
-          
+
 def install_cairo():
     """following instructions at:
         http://agiletesting.blogspot.com/2011_04_01_archive.html
         http://graphite.readthedocs.org/en/latest/install.html"""
-    sudo('yum -y install pkgconfig libpng libpng-devel freetype freetype-devel')
-    with cd('/tmp'):                                                      
+    sudo('yum -y install pkgconfig libpng-devel freetype-devel')
+    with cd('/tmp'):
         # install pixman
         run('wget http://cairographics.org/releases/pixman-0.20.2.tar.gz')
         run('tar xfz pixman-0.20.2.tar.gz')
@@ -51,7 +49,7 @@ def install_cairo():
         with cd('cairo-1.10.2'):
             with prefix('export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig'):
                 run('./configure --enable-xml && make')
-            sudo('make install')     
+            sudo('make install')
     # install py2cairo
     with prefix('workon %(venv_name)s' % env):
         with prefix('export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig'):
@@ -65,28 +63,44 @@ def install_graphite():
         run('pip install http://launchpad.net/graphite/0.9/0.9.8/+download/carbon-0.9.8.tar.gz')
         run('pip install http://launchpad.net/graphite/0.9/0.9.8/+download/whisper-0.9.8.tar.gz')
         run('pip install http://launchpad.net/graphite/0.9/0.9.8/+download/graphite-web-0.9.8.tar.gz')
-        django_root = run("""python -c 'from pkg_resources import get_distribution; print get_distribution("django").location'""")
-    with cd('/opt/graphite/conf'):
-        run('cp graphite.wsgi.example graphite.wsgi')
-        run('cp carbon.conf.example carbon.conf')
-        run('cp storage-schemas.conf.example storage-schemas.conf')
-    with cd('graphite-web-0.9.8'):
-        sed('examples/example-graphite-vhost.conf', '@DJANGO_ROOT@', django_root)
-        run("sed -i '1iLoadModule wsgi_module modules/mod_wsgi.so' examples/example-graphite-vhost.conf")
-        sudo('cp examples/example-graphite-vhost.conf /etc/httpd/conf.d/graphite.conf')
 
 def install_mod_wsgi():
-    if not exists('/usr/lib64/httpd/modules/mod_wsgi.so'):
-        with cd('/tmp'):
-            sudo('wget http://modwsgi.googlecode.com/files/mod_wsgi-3.3.tar.gz')
-            sudo('tar -xzvf mod_wsgi-3.3.tar.gz')
-            with cd('mod_wsgi-3.3'):
-                sudo('./configure -with-python=/usr/bin/python2.6')
-                sudo('make')
-                sudo('make install')
-            sudo('rm -rf mod_wsgi-3.3.tar.gz mod_wsgi-3.3')
-    else:
-        run('echo "mod_wsgi already installed"')
+    with cd('/tmp'):
+        sudo('wget http://modwsgi.googlecode.com/files/mod_wsgi-3.3.tar.gz')
+        sudo('tar -xzvf mod_wsgi-3.3.tar.gz')
+        with cd('mod_wsgi-3.3'):
+            sudo('./configure -with-python=/usr/bin/python2.6')
+            sudo('make')
+            sudo('make install')
+        sudo('rm -rf mod_wsgi-3.3.tar.gz mod_wsgi-3.3')
+
+def configure():
+    # get latest config in place
+    run('wget https://bitbucket.org/svtidevelopers/statistik/get/tip.tar.gz')
+    run('tar xfz tip.tar.gz')
+    with cd('statistik'):
+        sudo('rm -rf /opt/config; mv config /opt/')
+    run('rm -rf tip.tar* statistik')
+    # convenience files
+    run('rm ~/.screenrc; ln -s /opt/config/screenrc ~/.screenrc')
+    run('rm ~/.bashrc; ln -s /opt/config/bashrc ~/.bashrc')
+    run('rm ~/.hgrc; ln -s /opt/config/hgrc ~/.hgrc')
+    # graphite config files
+    sudo('rm /opt/graphite/conf/graphite.wsgi; ln -s /opt/config/opt/graphite/conf/graphite.wsgi /opt/graphite/conf/graphite.wsgi')
+    sudo('rm /opt/graphite/conf/carbon.conf; ln -s /opt/config/opt/graphite/conf/carbon.conf /opt/graphite/conf/carbon.conf')
+    sudo('rm /opt/graphite/conf/storage-schemas.conf; ln -s /opt/config/opt/graphite/conf/storage-schemas.conf /opt/graphite/conf/storage-schemas.conf')
+    sudo('rm /opt/graphite/conf/dashboard.conf; ln -s /opt/config/opt/graphite/conf/dashboard.conf /opt/graphite/conf/dashboard.conf')
+    # apache2 config files
+    sudo('rm /etc/httpd/conf/httpd.conf; ln -s /opt/config/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf')
+    sudo('rm /etc/httpd/conf.d/graphite.conf; ln -s /opt/config/etc/httpd/conf.d/graphite.conf /etc/httpd/conf.d/graphite.conf')
+    with prefix('workon %(venv_name)s' % env):
+        python_root = run("""python -c 'from pkg_resources import get_distribution; print get_distribution("django").location'""")
+        django_root = run("""cd /; python -c 'print __import__("django").__path__[0]'""")
+        print django_root
+        print python_root
+    with cd('/etc/httpd/conf.d/'):
+        sed('graphite.conf', '@DJANGO_ROOT@', django_root, use_sudo=True)
+        sed('graphite.conf', '@PYTHON_ROOT@', python_root, use_sudo=True)
 
 def setup():
     """
@@ -95,10 +109,10 @@ def setup():
     """
     require('hosts', provided_by=[ stage ])
     # install required packages
-    sudo('yum -y install screen mlocate make gcc python-devel')
+    sudo('yum -y install screen mlocate make gcc python-devel mercurial')
     sudo('/usr/bin/updatedb')
-    put(os.path.join(os.path.expanduser('~'), '.screenrc'))
 #   install_mod_wsgi
 #   install_virtualenv()
 #   install_pycairo()
-    install_graphite()
+#    install_graphite()
+    configure()
